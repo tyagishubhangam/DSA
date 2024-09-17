c6efa2345cc030f7188dc97ffc14053bcc6badb0
tyagishubhangam <143538952+tyagishubhangam@users.noreply.github.com>
2024-09-18 00:32:16 +0530

    Time: 0 ms (100%), Space: 41.1 MB (80.58%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -3,16 +3,7 @@ class Solution {
         if(s.length() != goal.length()){
             return false;
         }
-        if(s.equals(goal)){
-            return true;
-        }
-        String temp = s;
-        for(int i=1;i<s.length();i++){
-            temp = temp.substring(1,s.length()) + temp.charAt(0);
-            // System.out.println(temp);
-            if(temp.equals(goal)){
-                return true;
-            }
-        }return false;
+        String temp = s + s;
+        return temp.contains(goal);
     }
 }
