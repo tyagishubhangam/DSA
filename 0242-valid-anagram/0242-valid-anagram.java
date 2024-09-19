5514816defc348e0bf464d051587129ff38160a2
tyagishubhangam <143538952+tyagishubhangam@users.noreply.github.com>
2024-09-20 04:15:42 +0530

    Time: 6 ms (43.22%), Space: 42.6 MB (95.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int arrs[] = new int[26];
+        int arrt[] = new int[26];
+        for(int i=0;i<s.length();i++){
+            int cs = (int)s.charAt(i) -97;
+            int ct = (int)t.charAt(i) -97;
+            arrs[cs] +=1;
+            arrt[ct] +=1;
+        }
+        for(int j=0;j<26;j++){
+            if(arrs[j] != arrt[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
