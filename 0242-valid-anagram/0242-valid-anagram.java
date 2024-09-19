422047a7da8b64a464495fb91a31f2122b1e5a7f
tyagishubhangam <143538952+tyagishubhangam@users.noreply.github.com>
2024-09-20 04:42:52 +0530

    Time: 6 ms (43.22%), Space: 42.6 MB (95.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,15 +4,15 @@ class Solution {
             return false;
         }
         int arrs[] = new int[26];
-        int arrt[] = new int[26];
+        // int arrt[] = new int[26];
         for(int i=0;i<s.length();i++){
             int cs = (int)s.charAt(i) -97;
             int ct = (int)t.charAt(i) -97;
             arrs[cs] +=1;
-            arrt[ct] +=1;
+            arrs[ct] -=1;
         }
         for(int j=0;j<26;j++){
-            if(arrs[j] != arrt[j]){
+            if(arrs[j] >0){
                 return false;
             }
         }
