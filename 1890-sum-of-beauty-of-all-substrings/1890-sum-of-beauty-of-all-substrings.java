1625ad54e656639888d11a61978b21dbb0829394
tyagishubhangam <143538952+tyagishubhangam@users.noreply.github.com>
2024-10-18 00:45:56 +0530

    Time: 64 ms (54.58%), Space: 42.5 MB (77.2%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -5,26 +5,29 @@ class Solution {
             int arr[] = new int[26];
             for(int j=i;j<s.length();j++){
                 arr[s.charAt(j) - 'a']++;
-                result += getMaxFreq(arr) - getMinFreq(arr);
+                // result += getMaxFreq(arr) - getMinFreq(arr);
+                result += getRes(arr);
             }
         }
         return result;
         
     }
-    public int getMinFreq(int arr[]){
-        int res = Integer.MAX_VALUE;
+    public int getRes(int arr[]){
+        int resMin = Integer.MAX_VALUE;
+        int resMax = 0;
         for(int i: arr){
+            resMax = Math.max(resMax, i);
             if(i != 0){
-                res = Math.min(res, i);
+                resMin = Math.min(resMin, i);
             }
         }
-        return res;
+        return resMax - resMin;
     }
-    public int getMaxFreq(int arr[]){
-        int res = 0;
-        for(int i : arr){
-            res = Math.max(res, i);
-        }
-        return res;
-    }
+    // public int getMaxFreq(int arr[]){
+    //     int res = 0;
+    //     for(int i : arr){
+    //         res = Math.max(res, i);
+    //     }
+    //     return res;
+    // }
 }
