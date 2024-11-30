c9fd0d184e76426956267a12a3c7aa1eb33b0b2b
tyagishubhangam <143538952+tyagishubhangam@users.noreply.github.com>
2024-12-01 05:03:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            // Convert ArrayList to array
+            Integer[] arr = new Integer[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call totalFruits method and print result
+            Integer ans = ob.totalFruits(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int totalFruits(Integer[] arr) {
+        // code here
+        
+        int maxLen = 0;
+        for(int i=0;i<arr.length;i++){
+            Set<Integer> st = new HashSet<>();
+            for(int j=i;j<arr.length;j++){
+                st.add(arr[j]);
+                if(st.size() <= 2){
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }else{
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
+}
