a989d8cea6b5a1335a95f5eef96e84790378bb1f
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-02-26 03:43:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+        sc.nextLine();        // Consume the newline character
+
+        while (t-- > 0) {
+            String input = sc.nextLine();
+
+            // Replace ][ with ],[
+            input = input.replace("][", "],[");
+
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+            String[] rows = input.split("],\\s*\\[");
+
+            for (String row : rows) {
+                row = row.replaceAll("[\\[\\]]", ""); // Remove any surrounding brackets
+                ArrayList<Integer> intRow = new ArrayList<>();
+                for (String num : row.split(",")) {
+                    intRow.add(Integer.parseInt(num.trim()));
+                }
+                mat.add(intRow);
+            }
+
+            Solution obj = new Solution();
+            int result = obj.rowWithMax1s(convertListToArray(mat));
+
+            System.out.println(result);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+
+    // Helper method to convert ArrayList<ArrayList<Integer>> to 2D array
+    public static int[][] convertListToArray(ArrayList<ArrayList<Integer>> mat) {
+        int rows = mat.size();
+        int cols = mat.get(0).size();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = mat.get(i).get(j);
+            }
+        }
+        return arr;
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        
+        int r = arr.length;
+        int c = arr[0].length;
+        int ans = -1;
+        int maxSum = 0;
+        for(int i=0;i<r;i++){
+            int rowSum = 0;
+            for(int j=0;j<c;j++){
+                rowSum += arr[i][j];
+            }
+            if(rowSum > 0 && rowSum > maxSum){
+                maxSum = rowSum;
+                ans = i;
+            }
+        }
+        
+        return ans;
+    }
+}
