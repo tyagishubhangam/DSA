e30f6830e9bb805fe39cfb0f77f4f6ce43d600e2
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-03-11 00:11:38 +0530

    Time: 80 ms (5.04%), Space: 45.6 MB (9.53%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        List<String> li = new ArrayList<>();
+        char[] arr = new char[n];
+        generateHappyStrings('a',arr,0,n,li);
+        generateHappyStrings('b',arr,0,n,li);
+        generateHappyStrings('c',arr,0,n,li);
+        System.out.println(li);
+        if(k>li.size()){
+            return "";
+        }
+        return li.get(k-1);
+    }
+
+    public void generateHappyStrings(char ch, char[] arr, int idx, int n,List<String> li){
+        arr[idx] = ch;
+        if(idx+1 == n){
+            String str = "";
+            for(char i : arr){
+                str += i;
+            }
+            li.add(str);
+            return;
+        }
+        
+
+        
+        if(ch == 'a'){
+            
+            generateHappyStrings('b',arr,idx + 1, n, li);
+            
+            generateHappyStrings('c',arr,idx + 1, n, li);
+
+        }else{
+            if(ch == 'b'){
+            
+                generateHappyStrings('a',arr,idx + 1, n, li);
+            
+                generateHappyStrings('c',arr,idx + 1, n, li);
+
+            }else{
+                generateHappyStrings('a',arr,idx + 1, n, li);
+            
+                generateHappyStrings('b',arr,idx + 1, n, li);
+            }
+        }
+    }
+}
