8f0c311607f4692fc477119d0986565fdc0e1626
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-03-12 15:28:30 +0530

    Time: 6 ms (34.55%), Space: 43.1 MB (37.91%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        generateIpAddrs(0,0,"",s,ans);
+        return ans;
+    }
+
+    public static void generateIpAddrs(int i, int dots,String tmp, String s, List<String> ans){
+        if(i == s.length() && dots == 4){
+            ans.add(tmp.substring(0,tmp.length() - 1));
+            return;
+        }
+
+        if(dots > 4){
+            return;
+        }
+
+        for(int j = i;j< Math.min(i+3,s.length());j++){
+            String nxt = s.substring(i,j+1);
+            if(nxt.length() == 1 || s.charAt(i) != '0'){
+                if(Integer.parseInt(nxt) < 256){
+                    generateIpAddrs(j+1,dots+1,tmp+nxt+".",s,ans);
+                }
+            }
+        }
+    }
+}
