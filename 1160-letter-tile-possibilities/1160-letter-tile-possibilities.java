829c5e9286708d3152277faaf6894e68f8c76896
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-03-21 03:40:46 +0530

    Time: 1 ms (85.8%), Space: 44.9 MB (36.66%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -1,19 +1,20 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
         char[] arr = tiles.toCharArray();
-        return getAns(arr,0,0);
+        return getAns(arr,0);
     }
 
-    public static int getAns(char[] arr, int i, int ans){
+    public static int getAns(char[] arr, int i){
         if(i == arr.length ){
-            return ans;
+            return 0;
         }
         
         int[] freq = new int[26];
+        int ans = 0;
         for(int j=i;j<arr.length;j++){
             if(freq[arr[j]-'A']==0){
                 swap(arr,i,j);
-                ans = 1+getAns(arr,i+1,ans);
+                ans += 1+getAns(arr,i+1);
                 swap(arr,i,j);
             }
             freq[arr[j]-'A']++;
