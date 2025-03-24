649b3f01dd24970a65d5c1a8ec7d379271279c48
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-03-24 15:20:39 +0530

    Time: 6 ms (98.65%), Space: 57 MB (44.84%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,52 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        int len = s.length();
+        int[][] states = new int[len][len];
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len;j++){
+                if(i>=j){
+                    states[i][j] = 1;
+                }
+            }
+        }
+
+        for(int i=1;i<len;i++){
+            int k = 0;
+            int j = i;
+            while(k<len && j< len){
+                if(states[k+1][j-1] == 1 && (s.charAt(k) == s.charAt(j))){
+                states[k][j] = 1;}
+                k++;
+                j++;
+            }
+        }
+        
         List<List<String>> li = new ArrayList<>();
         List<String> tmp = new ArrayList<>();
-        generatePalins(s,tmp,0,li);
+        generatePalins(s,tmp,0,li,states);
         return li;
 
     }
-    public static void generatePalins(String s, List<String> tmp, int i, List<List<String>> li){
+    public static void generatePalins(String s, List<String> tmp, int i, List<List<String>> li, int[][] states){
         if(i == s.length()){
             li.add(new ArrayList<>(tmp));
             return;
         }
 
         for(int j = i;j<s.length();j++){
-            if(isPalin(s,i,j)){
+            if(isPalin(states,i,j)){
                 tmp.add(s.substring(i,j+1));
-                generatePalins(s,tmp,j+1,li);
+                generatePalins(s,tmp,j+1,li,states);
                 tmp.removeLast();
             }
         }
 
     }
 
-    public static boolean isPalin(String s, int i, int j){
-        while(i<=j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+    public static boolean isPalin(int[][] states, int i, int j){
+        
+        return states[i][j] == 1;
     }
+
+    
 }
