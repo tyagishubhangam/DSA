c577754b5a793b40e0c11c715ab2d85aa6641612
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-09 23:32:29 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,27 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        
-       double ans = getAns(x,Math.abs(n));
-       if(n < 0){
-        return 1/ans;
-       }
-       return ans;
+       double ans = getAns(x,n);
+      return n<0?(1/ans):ans;
 
-        
     }
 
     public static double getAns(double x, int n){
          if(n == 0){
             return 1;
         }
-
-
-        double res = (getAns(x,n/2));
-            if(n % 2 == 0){
-                
-                    return (res * res);
-            }
-            return( res * res* x);
-        
+        double tmp = getAns(x, n/2);
+        if(n % 2 != 0){
+            return x * tmp*tmp;
+        }
+        return tmp * tmp;
     }
 }
