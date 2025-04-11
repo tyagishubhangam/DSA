d19d80c10a2d901a8dcf93fec4fa40559a375f89
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-11 15:53:13 +0530

    Time: 27 ms (22.57%), Space: 51 MB (55.99%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -4,20 +4,20 @@ class Solution {
         for(int i=0;i<text1.length();i++){
             Arrays.fill(mem[i], -1);
         }
-        return getAns(text1, text2, 0,0, mem);
+        return getAns(text1, text2, text1.length() -1,text2.length() -1, mem);
     }
 
     public static int getAns(String text1, String text2, int i, int j, int[][]mem){
-        if(i == text1.length() || j == text2.length()){
+        if(i == -1 || j == -1){
             return 0;
         }
         if(mem[i][j] != -1){
             return mem[i][j];
         }
         if(text1.charAt(i) == text2.charAt(j)){
-            return 1 + getAns(text1, text2, i+1, j+1, mem);
+            return 1 + getAns(text1, text2, i-1, j-1, mem);
         }
-            return mem[i][j] = Math.max(getAns(text1, text2, i, j+1, mem),getAns(text1, text2, i+1, j, mem));
+            return mem[i][j] = Math.max(getAns(text1, text2, i, j-1, mem),getAns(text1, text2, i-1, j, mem));
         
     }
 }
