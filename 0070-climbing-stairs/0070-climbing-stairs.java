aae115cfff95a6f60087ae1b7e47f83c38fe631b
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-23 14:09:52 +0530

    Time: 0 ms (100%), Space: 40.6 MB (28.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
-        return recurse(0, n);
+        int[] dp = new int[n];
+        for(int i=0;i<n;i++){
+            dp[i] = -1;
+        }
+
+        return jump(0,dp, n);
     }
 
-    public static int recurse(int steps, int n){
-        if(steps == n){
-            return 1;
-        }
-        if(steps > n ){
+    public static int jump(int i, int[] dp, int n){
+        
+        if(i > n){
             return 0;
         }
-        return recurse(steps+1,n) + recurse(steps + 2, n);
+        if(i == n){
+            return 1;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        dp[i] = jump(i+1,dp, n) + jump(i+2, dp, n);
+
+        return dp[i];
+        
     }
 }
