30b29ab3f31dbdf192ec6e7153932693a6d1ed58
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-26 11:16:02 +0530

    Time: 1 ms (62.62%), Space: 46 MB (88.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int[] maxHeightLeft = new int[height.length];
-        int[] maxHeightRight = new int[height.length];
-        maxHeightLeft[0] = height[0];
-        for(int i=1;i<height.length;i++){
-            maxHeightLeft[i] = Math.max(maxHeightLeft[i-1], height[i]);
+        int n = height.length;
+        int[] leftHeights = new int[n];
+        int[] rightHeights = new int[n];
+        leftHeights[0] = height[0];
+        rightHeights[n-1] = height[n-1];
+        for(int i=1;i<n;i++){
+            leftHeights[i] = Math.max(height[i], leftHeights[i-1]);
         }
-        maxHeightRight[height.length - 1] = height[height.length - 1];
-        for(int i=height.length-2; i>=0;i--){
-            maxHeightRight[i] = Math.max(maxHeightRight[i+1], height[i]);
+        for(int i=n-2;i>=0;i--){
+            rightHeights[i] = Math.max(height[i], rightHeights[i+1]);
         }
-        int totalHeight = 0;
-        for(int i=1;i<height.length - 1;i++){
-            int minHeight = Math.min(maxHeightLeft[i-1], maxHeightRight[i+1]);
-            if(minHeight > height[i]){
-                totalHeight += minHeight - height[i];
+        int totHeight = 0;
+        for(int i=1;i<n-1;i++){
+            int tmp = Math.min(rightHeights[i+1],leftHeights[i-1]);
+            if(tmp > height[i]){
+                totHeight += tmp - height[i];
             }
         }
-        return totalHeight;
+        return totHeight;
     }
 }
