7d9d84a5e881468c2ba533d69fd504c05ea97659
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-30 02:21:29 +0530

    Time: 26 ms (31.43%), Space: 45.6 MB (12.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp,amount + 1);
-        dp[0] = 0;
-        for(int i=0;i<=amount;i++){
-            for(int coin:coins){
-                if(coin<= i){
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        long[] dp = new long[amount + 1];
+        Arrays.fill(dp, -1);
+        long res = getMinCoins(amount, coins,dp);
+        if(res >= Integer.MAX_VALUE){
+            return -1;
         }
-        return dp[amount] > amount ? -1:dp[amount];
+
+        return (int)res;
+    }
+
+    public static long getMinCoins(int amt, int[] coins,long[] dp){
+        if(amt == 0){
+            return 0;
+        }
+        if(amt < 0){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[amt] != -1){
+            return dp[amt];
+        }
+        long ans = Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++){
+            ans = Math.min(ans, 1+getMinCoins(amt - coins[i], coins, dp));
+        }
+
+        return dp[amt] = ans;
     }
 }
