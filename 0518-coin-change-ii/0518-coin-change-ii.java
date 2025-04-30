5d695c51bff443c043e91a724a7eb663cd280fcc
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-04-30 11:35:39 +0530

    Time: 9 ms (63.18%), Space: 49.1 MB (49.86%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,13 +1,26 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] += dp[i - coin];
-            }
+        int[][] dp = new int[coins.length][amount + 1];
+        for(int i=0;i<coins.length;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return getCount(amount,coins.length - 1, coins, dp);
+    }
+
+    public static int getCount(int amt, int i, int[] coins, int[][] dp){
+        if(amt == 0){
+            return 1;
+        }
+        if(amt < 0){
+            return 0;
+        }
+        if(i<0){
+            return 0;
         }
 
-        return dp[amount];
+        if(dp[i][amt] != -1){
+            return dp[i][amt];
+        }
+       return dp[i][amt] = getCount(amt - coins[i], i,coins, dp) + getCount(amt,i-1 ,coins,dp);
     }
 }
