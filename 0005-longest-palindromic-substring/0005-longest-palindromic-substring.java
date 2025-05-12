17531ffb122bee5ab4332df9df83ac20b1532994
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-05-13 04:58:27 +0530

    Time: 165 ms (28.6%), Space: 46.5 MB (14.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -10,16 +10,18 @@ class Solution {
         }
         int ans = 0;
         String str = s.substring(0,1);
-
+        int start = 0, end = 0;
         for(int k=1;k<=n;k++){
             int i=0;
             int j = k;
+            
             while(i < n && j < n){
                 if((s.charAt(i) == s.charAt(j)) && dp[i+1][j-1]){
                     dp[i][j] = true;
                     if(ans < (j - i + 1)){
                         ans  = j - i + 1;
-                        str = s.substring(i, j+1);
+                        start = i;
+                        end = j;
                     }
                 }else{
                     dp[i][j] = false;
@@ -28,7 +30,7 @@ class Solution {
                 j++;
             }
         }
-        System.out.println(ans);
-        return str;
+
+        return s.substring(start, end +1);
     }
 }
