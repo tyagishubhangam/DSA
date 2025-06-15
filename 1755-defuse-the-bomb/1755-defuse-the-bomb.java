28a66bfe1d05c5b83f3b49ac31ac18f21f5f2fa6
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-06-16 02:49:01 +0530

    Time: 1 ms (64.37%), Space: 42.8 MB (9.78%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,65 +1,50 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n = code.length;
-        int arr[] = new int[n];
+        int[] ans = new int[code.length];
         if(k == 0){
-            return arr;
+            return ans;
         }
+        int n = code.length;
+        int[] preSumLeft = new int[n+Math.abs(k)];
+        int[] preSumRight = new int[n+Math.abs(k)];
+        preSumRight[0] = code[0];
+        preSumLeft[0] = code[n - Math.abs(k)];
+
+
+        for(int i=1;i<preSumRight.length;i++){
+            if(i<code.length){
+                preSumRight[i] += preSumRight[i-1] + code[i];
+            }else{
+                if(i>=code.length){
+                    preSumRight[i] += preSumRight[i-1] + code[i-code.length]; 
+                }
+            }
+
+            if(i<Math.abs(k)){
+                preSumLeft[i] = preSumLeft[i - 1] + code[n - Math.abs(k) + i];
+            }else{
+                if(i>=Math.abs(k)){
+                    preSumLeft[i] = preSumLeft[i - 1] + code[i - Math.abs(k)]; 
+                }
+            }
+        }
+
+
         if(k>0){
-        return kPositive(code, k);}
-        return kNegative(code, k);
+            for(int i = 0;i<n;i++){
+                ans[i] = preSumRight[i+k] - preSumRight[i];
+            }
+            // return ans;
+        }else{
+            for(int i=0;i<n;i++){
+                if(i == 0){
+                    ans[i] = preSumLeft[i+Math.abs(k)] - code[i];
+                }else{
+                    ans[i] = preSumLeft[i+Math.abs(k)] - preSumLeft[i - 1] - code[i];
+                }
+            }
         }
-
-
-        public int[] kPositive(int[] code, int k){
-            int n = code.length;
-            int arr[] = new int[n];
-            int sum = 0, l = 0, r = 1;
-            for(int i=0;i<k;i++){
-            sum += code[r];
-            r++;
-            }
-            arr[l] = sum;
-            l = 1;
-            while(l < n){
-                // if(r >= n){
-                //     r = 0;
-                // }
-                r = r % n;
-                sum -= code[l];
-                sum += code[r];
-                arr[l] = sum;
-                l++;
-                r++;
-            }
-            return arr;
-        }
-
-        public int[] kNegative(int[] code, int k){
-            int sum = 0;
-            
-            int n = code.length;
-            int arr[] = new int[n];
-            k = Math.abs(k);
-            for(int i=n-k;i<n;i++){
-                sum += code[i];
-            }
-            arr[0] = sum;
-            int l = 0;
-            int r = n-k;
-            while(l<n-1){
-                r = r % n;
-                sum -= code[r];
-                sum += code[l];
-                l++;
-                r++;
-                arr[l] = sum;
-
-            }
-            return arr;
-
-        }
-
         
-    
+        return ans;
+    }
 }
