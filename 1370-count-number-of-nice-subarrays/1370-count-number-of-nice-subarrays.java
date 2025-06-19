babd09659c5e78178705431662ba77efa15a02b2
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-06-19 23:55:57 +0530

    Time: 9 ms (85.32%), Space: 55.2 MB (48.35%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,15 +1,37 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> mpp = new HashMap<>();
-        mpp.put(0,1);
-        int count = 0;
-        int preSum = 0;
-        for(int i=0;i<nums.length;i++){
-            preSum += nums[i] % 2;
-            count += mpp.getOrDefault(preSum - k, 0);
-            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        int n = nums.length;
+        int countEvens = 0;
+        int l = 0;
+        int r = 0;
+        int countOdds = 0;
+        int ans = 0;
+        while(r < n){
+            if(nums[r] % 2 != 0){
+                countOdds += 1;
+            }
+
+            while(l<r && ((nums[l] % 2 == 0) || countOdds > k)){
+                if(nums[l] % 2 == 0){
+                    countEvens++;
+                }else{
+                    countEvens = 0;
+                }
+
+                if(nums[l] % 2 != 0){
+                    countOdds -= 1;
+                }
+                l++;
+            }
+            if(countOdds == k){
+                ans += 1 + countEvens;
+            }
+            r++;
         }
-        return count;
-        
+
+        return ans;
     }
 }
+
+
+// isko same approach se kara hai as leetcode 930. Binary Subarrays With Sum
