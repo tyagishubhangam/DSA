f1e934406524c9ba2768c4b130d581ff9f28b1d8
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-06-19 23:33:20 +0530

    Time: 3 ms (51.71%), Space: 47.4 MB (84.21%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,15 +1,29 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int count = 0;
-        int preSum = 0;
-        Map<Integer, Integer> mpp = new HashMap<>();
-        mpp.put(0, 1);
-        for(int i=0;i<nums.length;i++){
-            preSum += nums[i];
-            int diff = preSum - goal;
-            count += mpp.getOrDefault(diff, 0);
-            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        int n = nums.length;
+        int count_zeros = 0;
+        int l = 0;
+        int r = 0;
+        int windowSum = 0;
+        int ans = 0;
+        while(r<n){
+            windowSum += nums[r];
+            while(l<r && (nums[l] == 0 || windowSum > goal)){
+                if(nums[l] == 0){
+                    count_zeros++;
+                }else{
+                    count_zeros = 0;
+                }
+                windowSum -= nums[l];
+                l++;
+            }
+            if(windowSum == goal){
+                ans += 1 + count_zeros;
+            }
+            r++;
         }
-        return count;
+        return ans;
     }
 }
+
+// isme window jab zero start hori hai tab humm dekh sakte hai ki hum uske no. of zeros ko count karke left pointer ko aage badha sakte hai and then ans mei add kar sakte hai no. of zeros ko agar window SUm abhi bhi valid hai
