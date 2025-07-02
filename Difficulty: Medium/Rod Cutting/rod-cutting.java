a0838a9fbe15819bdef9a02138fe7f9813826651
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-07-03 01:06:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -1,54 +1,36 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class RodCutting {
-
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            String s[] = in.readLine().trim().split(" ");
-            int[] arr = new int[s.length];
-            for (int i = 0; i < s.length; i++) arr[i] = Integer.parseInt(s[i]);
-
-            Solution ob = new Solution();
-            out.println(ob.cutRod(arr));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int cutRod(int[] price) {
         // code here
+        int n = price.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i], -1);
+        }
         
-        int[] mem = new int[price.length + 1];
-        Arrays.fill(mem, -1);
-        return getAns(price.length, price, mem);
+        return recurse(n-1,price,n,dp);
         
     }
     
-    public static int getAns(int i, int[] price, int[] mem){
-        if(i == 0){
+    public static int recurse(int i, int[] price, int len, int[][] dp){
+        if(i<0){
             return 0;
         }
-        
-        if(mem[i] != -1){
-            return mem[i];
+        if(len < 0){
+            return 0;
+        }
+        if(len == 0){
+            return 0;
+        }
+        if(dp[i][len] != -1){
+            return dp[i][len];
+        }
+        int take = 0;
+        if((len-i-1) >=0){
+            take = price[i] + recurse(i,price,len - i-1,dp);
         }
         
-        for(int j=1;j<=i;j++){
-            mem[i] = Math.max(mem[i], price[j-1] + getAns(i-j, price, mem));
-        }
+        int notTake = recurse(i-1,price,len,dp);
+        return dp[i][len]=Math.max(take,notTake);
         
-        return mem[i];
     }
 }
