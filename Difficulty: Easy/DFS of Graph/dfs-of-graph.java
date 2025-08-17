fad4fdaf6ecfed1687c76aecc67c6923d81fe9a2
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-18 04:39:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -2,26 +2,30 @@ class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
         // Code here
+        int n = adj.size();
+        boolean[] visited = new boolean[n];
         ArrayList<Integer> res = new ArrayList<>();
-        ArrayList<Boolean> visited = new ArrayList<>();
-        for(int i = 0;i<adj.size();i++){
-            visited.add(false);
-        }
-        DFS(adj,0,visited,res);
-        return res;
-    }
-    
-    public static void DFS(ArrayList<ArrayList<Integer>> adj,int u, ArrayList<Boolean> visited, ArrayList<Integer> res){
-        if(visited.get(u)){
-            return;
-        }
-        visited.set(u,true);
-        res.add(u);
-        
-        for(int v:adj.get(u)){
-            if(!visited.get(v)){
-                DFS(adj,v,visited,res);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                doDFS(adj, i, visited, res);
             }
         }
+        return res;
+        
+    }
+    
+    public static void doDFS(ArrayList<ArrayList<Integer>> adj, int u,
+    boolean[] visited, ArrayList<Integer> res){
+        if(visited[u]){
+            return ;
+        }
+        res.add(u);
+        visited[u] = true;
+        for(int v:adj.get(u)){
+            if(!visited[v]){
+                doDFS(adj, v, visited, res);
+            }
+        }
+        
     }
 }
