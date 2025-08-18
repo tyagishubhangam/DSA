33612250082de387229855bd06711b8c14a81bfc
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-19 01:23:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,11 +1,14 @@
+
+
+
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
         // Code here
         List<List<Integer>> adj = new ArrayList<>();
+        
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
@@ -13,30 +16,49 @@ class Solution {
         
         boolean[] visited = new boolean[V];
         for(int i=0;i<V;i++){
-            if(!visited[i] && DFS(adj, i, visited, -1)){
+            if(!visited[i] && checkCycleBFS(adj, i, visited)){
                 return true;
+            }
+        }
+        
+        return false;
+        
+        
+    }
+    
+    public static boolean checkCycleBFS(List<List<Integer>> adj, int i, boolean[] visited){
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(i, -1));
+        visited[i] = true;
+        
+        while(!queue.isEmpty()){
+            Pair node = queue.poll();
+            int u = node.u;
+            int parent = node.parent;
+            
+            for(int v : adj.get(u)){
+                if(v == parent){
+                    continue;
+                }
+                
+                if(visited[v]){
+                    return true;
+                }
+                visited[v] = true;
+                queue.offer(new Pair(v, u));
             }
         }
         
         return false;
     }
     
-    public static boolean DFS(List<List<Integer>> adj, int u, boolean[] visited,int parent){
-        
-        visited[u] = true;
-        
-        for(int v : adj.get(u)){
-            if(v == parent){
-                continue;
-            }
-            if(visited[v]) {
-                return true;
-            }
-            if(DFS(adj, v, visited,u)){
-                return true;
-            }
-            
-        }
-        return false;
+}
+
+class Pair{
+    int u;
+    int parent;
+    Pair(int u, int parent){
+        this.u = u;
+        this.parent = parent;
     }
 }
