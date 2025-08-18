483fbf88eb16ae9ed5d2f33d32365cf7d554a601
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-19 00:59:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -5,16 +5,15 @@ class Solution {
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] edge:edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        boolean[] visited = new boolean[V];
         
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
+        boolean[] visited = new boolean[V];
         for(int i=0;i<V;i++){
-            if(!visited[i] && checkCycleBFS(adj,i,visited)){
+            if(!visited[i] && DFS(adj, i, visited, -1)){
                 return true;
             }
         }
@@ -22,38 +21,22 @@ class Solution {
         return false;
     }
     
-    public static boolean checkCycleBFS(List<List<Integer>> adj,int u, boolean[] visited){
+    public static boolean DFS(List<List<Integer>> adj, int u, boolean[] visited,int parent){
+        
         visited[u] = true;
         
-        Queue<int[]> queue = new LinkedList<>();
-       int[] pair = new int[2];
-       pair[0] = u;
-       pair[1] = -1;
-        queue.add(pair);
-        
-        while(!queue.isEmpty()){
-            int[] x = queue.poll();
-            int node = x[0];
-            int parent = x[1];
-            for(int v:adj.get(node)){
-                if(parent == v){
-                    continue;
-                }
-                if(visited[v]){
-                    return true;
-                }
-                // Pair<Integer, Integer> tmp = new Pair<>(v,node);
-                int[] tmp = new int[2];
-                tmp[0] = v;
-                tmp[1] = node;
-                queue.add(tmp);
-                visited[v] = true;
+        for(int v : adj.get(u)){
+            if(v == parent){
+                continue;
             }
+            if(visited[v]) {
+                return true;
+            }
+            if(DFS(adj, v, visited,u)){
+                return true;
+            }
+            
         }
-        
         return false;
-        
-        
-        
     }
 }
