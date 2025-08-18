43fbd9a2eaa3230a2deb17c86abc8458978f518c
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-19 04:00:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -2,34 +2,46 @@ class Solution {
     public static ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
         List<List<Integer>> adj = new ArrayList<>();
+        int[] inDegree = new int[V];
+        
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
         
         for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
+            inDegree[edge[1]]++;
         }
         ArrayList<Integer> result = new ArrayList<>();
-        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(!visited[i]){
-                DFS(adj, i, visited, result);
+            if(inDegree[i] == 0){
+                queue.offer(i);
             }
         }
-        Collections.reverse(result);
+        
+        while(!queue.isEmpty()){
+            int u = queue.poll();
+            result.add(u);
+            for(int v : adj.get(u)){
+                if(inDegree[v] == 0){
+                    queue.offer(v);
+                }else{
+                    inDegree[v]--;
+                    if(inDegree[v] == 0){
+                        queue.add(v);
+                    }
+                }
+            }
+        }
+        
+        while(!queue.isEmpty()){
+            result.add(queue.poll());
+        }
+        // System.out.println(result);
         
         return result;
     }
     
-    public static void DFS(List<List<Integer>> adj, int u, boolean[] visited, ArrayList<Integer> result){
-        visited[u] = true;
-        for(int v : adj.get(u)){
-            if(!visited[v]){
-                DFS(adj, v, visited, result);
-            }
-        }
-        
-        result.add(u);
-        
-    }
+    
 }
