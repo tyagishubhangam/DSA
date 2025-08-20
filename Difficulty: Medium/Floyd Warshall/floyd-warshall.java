ef5c11c4953498f3ec14e31b559fa16aa9b42b15
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-21 01:17:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -5,10 +5,8 @@ class Solution {
         // Code here
         int n = dist.length;
         for(int i=0;i<n;i++){
-            for(int j = 0;j<n;j++){
+            for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
-                    
-                    
                     if(dist[j][i] < (int)1e8 && dist[i][k] < (int)1e8){
                         dist[j][k] = Math.min(dist[j][k], dist[j][i] + dist[i][k]);
                     }
