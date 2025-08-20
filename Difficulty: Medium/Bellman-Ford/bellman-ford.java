1277addb9664cf7a66cca10626db1a59bf26fd2a
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-20 18:49:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.java
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -6,32 +6,26 @@ class Solution {
         int[] dist = new int[V];
         Arrays.fill(dist, (int)1e8);
         dist[src] = 0;
-        
-        
         for(int i=1;i<V;i++){
-            for(int[] edge:edges){
+            for(int[] edge : edges){
                 int u = edge[0];
                 int v = edge[1];
-                int w = edge[2];
-                
-                if(dist[u] != (int)1e8 && (dist[u] + w) < dist[v]){
-                    dist[v] = dist[u] + w;
+                int wt = edge[2];
+                if(dist[u] != (int)1e8 && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
                 }
             }
         }
         
-        for(int[] edge:edges){
-                int u = edge[0];
-                int v = edge[1];
-                int w = edge[2];
-                
-                if(dist[u] != (int)1e8 && (dist[u] + w) < dist[v]){
-                    
-                    return new int[]{-1};
-                }
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            if(dist[u] != (int)1e8 && dist[u]+wt < dist[v]){
+                return new int[]{-1};
             }
-            
-            return dist;
+        }
         
+        return dist;
     }
 }
