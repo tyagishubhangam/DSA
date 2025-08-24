a31ee726da39e8b59c679d184eb82822860032e7
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-08-24 22:47:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,44 +1,56 @@
 class Solution {
+    int[] parent;
+    int[] rank;
+    
+    public int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    public void union(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+        if(parent_a == parent_b){
+            return ;
+        }
+        if(rank[parent_a] == rank[parent_b]){
+            parent[parent_b] = parent_a;
+            rank[parent_a]++;
+        }else{
+            if(rank[parent_a] > rank[parent_b]){
+                parent[parent_b] = parent_a;
+            }else{
+                parent[parent_a] = parent_b;
+            }
+        }
+    }
     public int spanningTree(int V, int[][] edges) {
-        // code here
-        List<List<int[]>> adj = new ArrayList<>();
+        rank = new int[V];
+        parent = new int[V];
         for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
+            parent[i] = i;
         }
-        for(int[] edge : edges){
-            adj.get(edge[0]).add(new int[]{edge[2], edge[1]});
-            adj.get(edge[1]).add(new int[]{edge[2], edge[0]});
-
-        }
-        boolean[] visited = new boolean[V];
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new SortThePQ());
-        pq.offer(new int[]{0,0});
-        int res = 0;
-        while(!pq.isEmpty()){
-            int[] tmp = pq.poll();
-            int wt = tmp[0];
-            
-            int u = tmp[1];
-            if(visited[u]){
-                continue;
+        int result = 0;
+       
+        Arrays.sort(edges,new SortTheNodes());
+        for(int[] tmp : edges){
+            int u = tmp[0];
+            int v = tmp[1];
+            int wt = tmp[2];
+            if(find(u) != find(v)){
+                union(u,v);
+                result += wt;
             }
-            res += wt;
-            visited[u] = true;
-            for(int[] child : adj.get(u)){
-                int v = child[1];
-                int weigh = child[0];
-                if(!visited[v]){
-                   pq.offer(new int[]{weigh, v});
-                }
-            }
-            
         }
-        return res;
+        return result;
     }
 }
 
-class SortThePQ implements Comparator<int[]>{
+class SortTheNodes implements Comparator<int[]>{
     public int compare(int[] a, int[] b){
-        return a[0] - b[0];
+        return a[2] - b[2];
     }
 }
