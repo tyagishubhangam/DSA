609927ccc53dbcbe29527089166e9c4faa5c66ac
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-09-06 03:23:23 +0530

    Time: 23 ms (18.88%), Space: 94.7 MB (59.56%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,23 +2,23 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> li = new ArrayList<>();
         List<Integer> tmp = new ArrayList<>();
-        getCombinations(1,n,k,tmp,li);
+        recurse(1,k,li, tmp, n);
         return li;
     }
 
-    public static void getCombinations(int idx,int n, int k, List<Integer> tmp, List<List<Integer>> li){
+    public void recurse(int i, int k, List<List<Integer>> li, List<Integer> tmp, int n){
         if(tmp.size() == k){
             li.add(new ArrayList<>(tmp));
             return;
         }
-        if(idx > n){
-            return;
+        if(tmp.size() > k || i > n){
+            return ;
         }
-
-        tmp.add(idx);
-        getCombinations(idx+1,n,k,tmp,li);
-        tmp.removeLast();
-        getCombinations(idx+1,n,k,tmp,li);
-            
+        tmp.add(i);
+        int idx = tmp.size()-1;
+        recurse(i+1, k,li,tmp,n);
+        tmp.remove(idx);
+        recurse(i+1, k, li, tmp, n);
+        
     }
 }
