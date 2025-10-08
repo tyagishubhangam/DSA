48a343216061c047089004c40a5e07f49c851959
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-10-09 02:13:07 +0530

    Time: 0 ms (100%), Space: 42.4 MB (39.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,43 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int t = 0;
-        int b = n - 1;
-        
-        while(t<=b){
-            int mid = (t + b) / 2;
-            int idx = binarySearch(matrix[mid], target);
-            if(idx != -1){
-                return true;
-            }else{
-                if(target > matrix[mid][m-1]){
-                    t = mid + 1;
-                }else{
-                    b = mid - 1;
-                }
-            }
-        }
-        return false;
-
-        
-    }
-
-    public static int binarySearch(int[] arr, int k){
+        int m = matrix.length;
+        int n = matrix[0].length;
         int l = 0;
-        int r = arr.length -1 ;
-        while(l<=r){
-            int mid = (l + r)/2;
-            if(arr[mid] == k){
-                return mid;
+        int r = m-1;
+        while(l <= r){
+            int mid = l + (r-l) / 2;
+            if(matrix[mid][0] == target){
+                return true;
+            }
+            if(matrix[mid][0] < target){
+                l = mid + 1;
             }else{
-                if(arr[mid] > k){
-                    r = mid -1 ;
-                }else{
-                    l = mid + 1;
-                }
+                r = mid - 1;
             }
         }
-        return -1;
+
+        if(r < 0 ){
+            return false;
+        }
+        int lr = 0;
+        int rr = n-1;
+        while(lr <= rr){
+            int mid = lr + (rr-lr)/2;
+            if(matrix[r][mid] == target){
+                return true;
+            }
+            if(matrix[r][mid] < target){
+                lr = mid + 1;
+            }else{
+                rr = mid -1;
+            }
+        }
+    return false;
     }
 }
