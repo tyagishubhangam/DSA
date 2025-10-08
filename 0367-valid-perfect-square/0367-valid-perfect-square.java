dc8a182b25f94ae3b55f9faec154bf2e6303b525
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-10-09 00:46:08 +0530

    Time: 0 ms (100%), Space: 40.8 MB (12.29%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        long l = 1;
-        long r = num;
-        long mid = (l + r) / 2;
-        
-        while(l<=r){
-            mid = (l + r) / 2;
-            if((mid*mid) == num){
+        int l = 1;
+        int r = num+1/2;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if((mid) == Math.sqrt(num)){
                 return true;
+            }
+            if((mid) > Math.sqrt(num)){
+                r = mid - 1;
             }else{
-                if((mid*mid) > num){
-                    r = mid - 1;
-                }else{
-                    l = mid + 1;
-                }
+                l = mid + 1;
             }
         }
         return false;
