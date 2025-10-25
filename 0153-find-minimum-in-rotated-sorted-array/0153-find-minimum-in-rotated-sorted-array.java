cca91ffbaba73dc8488f36ba9cbbf5bc0933c02c
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-10-26 02:51:35 +0530

    Time: 0 ms (100%), Space: 42.6 MB (7.34%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,32 +1,45 @@
 class Solution {
     public int findMin(int[] nums) {
         int n = nums.length;
-        // if(n == 1){
-        //     return nums[0];
-        // }
-        if(nums[0] <= nums[n - 1]){
+        if(n == 1){
             return nums[0];
         }
         int l = 0;
         int r = n - 1;
-        int mid = (l + r) / 2;
-        int pivot = 0;
-        while(l<=r){
-            mid = (l + r) / 2;
-            if(nums[mid] > nums[mid + 1] ){
-                pivot = mid;
-                break;
-            }else{
-                if(nums[mid] > nums[n - 1]){
-                    l = mid + 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(mid == 0){
+                if(nums[mid] > nums[mid+1]){
+                    return nums[mid+1];
+                }
+                if(nums[mid] < nums[mid+1] && nums[mid] < nums[n-1]){
+                    return nums[mid];
                 }else{
-                    r = mid - 1;
+                    l = mid + 1;
+                }
+
+            }else{
+                if(mid == n-1){
+                    if(nums[mid] < nums[mid - 1] && nums[mid] < nums[0]){
+                        return nums[mid];
+                    }else{
+                        r = mid - 1;
+                    }
+                }else{
+                    if(nums[mid] > nums[mid + 1]){
+                        return nums[mid+1];
+                    }
+                    if(nums[mid] < nums[mid-1]){
+                        return nums[mid];
+                    }
+                    if(nums[mid] < nums[mid+1] && nums[mid] > nums[n-1]){
+                        l = mid + 1;
+                    }else{
+                        r = mid - 1;
+                    }
                 }
             }
         }
-        
-           return nums[pivot+1]; 
-        
-
+        return 0;
     }
 }
