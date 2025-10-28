8b4bc3ab88d95077da02b5608da6351ce5bf80bf
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-10-28 23:44:37 +0530

    Time: 13 ms (55.13%), Space: 45 MB (77.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,31 +1,31 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int total = 0;
         int n = piles.length;
-        int r = 0;
+        long l = 1;
+        long r = 0;
         for(int i=0;i<n;i++){
-            total += piles[i];
-            r = Math.max(r,piles[i]);
+            r = Math.max(r, piles[i]);
         }
-        int l = (int)Math.ceil(total*1.0 / h);
-        int ans = Integer.MAX_VALUE;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(isValid(h,piles, mid)){
+        while(l <= r){
+            long mid = l + (r - l) / 2;
+            long time = getTotalTimeToEat(piles, mid);
+            if(time <= h){
                 r = mid - 1;
-                ans = Math.min(ans, mid);
             }else{
                 l = mid + 1;
             }
         }
-        return ans;    
+        return (int)l;
     }
+    public long getTotalTimeToEat(int[] piles, long cnt){
+        long tmp = 0;
+        for(int pile : piles){
+            tmp += (long)pile / cnt;
+            if(pile % cnt != 0){
+                tmp += 1;
+            }
 
-    public boolean isValid(int k, int[] piles, int consumed){
-        int hrs = 0;
-        for(int i : piles){
-            hrs += (int)Math.ceil(i*1.0 / consumed);
+            }
+            return tmp;
         }
-        return hrs <= k;
     }
-}
