309b75edc4fae3482b35f81cdef51c31cb055caa
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-11-20 01:47:58 +0530

    Time: 3 ms (36.48%), Space: 47.1 MB (7.35%) - LeetHub

---
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int l = 0;
+        int r = 0;
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        for(int[] num : nums1){
+            hs.put(num[0], hs.getOrDefault(num[0], 0)+num[1]);
+        }
+        for(int[] num : nums2){
+            hs.put(num[0], hs.getOrDefault(num[0], 0)+num[1]);
+        }
+        int[][] ans = new int[hs.size()][2];
+        int idx = 0;
+        while(l < n && r < m){
+            if(nums1[l][0] == nums2[r][0]){
+                ans[idx][0] = nums1[l][0];
+                ans[idx][1] = hs.get(nums1[l][0]);
+                l++;
+                r++;
+            }else{
+                if(nums1[l][0] < nums2[r][0]){
+                    ans[idx][0] = nums1[l][0];
+                    ans[idx][1] = hs.get(nums1[l][0]);
+                    l++;
+                }else{
+                    ans[idx][0] = nums2[r][0];
+                    ans[idx][1] = hs.get(nums2[r][0]);
+                    r++;
+                }
+            }
+
+            idx++; 
+        }
+
+        while(l < n){
+            ans[idx][0] = nums1[l][0];
+            ans[idx++][1] = hs.get(nums1[l][0]);
+            l++;
+        }
+        while(r < m){
+            ans[idx][0] = nums2[r][0];
+            ans[idx++][1] = hs.get(nums2[r][0]);
+            r++;
+        }
+
+        return ans;
+
+    }
+}
