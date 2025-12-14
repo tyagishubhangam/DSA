c0c0b127931a5517833b3fc04dab05ee41f84401
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2025-12-15 01:01:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
@@ -1,5 +1,4 @@
-/*Complete the function below
-Node is as follows:
+/*
 class Node{
     int data;
     Node left,right;
@@ -10,41 +9,33 @@ class Node{
         left = right = null;
     }
 }
-
 */
 class Solution {
-    // Function to check whether all nodes of a tree have the value
-    // equal to the sum of their child nodes.
-    public static int isSumProperty(Node root) {
-        // add your code here
-        if(recurse(root)){
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    public static boolean recurse(Node root){
+    public boolean isSumProperty(Node root) {
+        //  code here
         if(root == null){
             return true;
         }
-        
         if(root.left == null && root.right == null){
             return true;
         }
-        int left = 0;
+        
+        int leftVal = 0;
+        int rightVal = 0;
         if(root.left != null){
-            left = root.left.data;
+            leftVal = root.left.data;
         }
-        int right = 0;
         if(root.right != null){
-            right = root.right.data;
+            rightVal = root.right.data;
         }
-        
-        if(left + right == root.data){
-            return recurse(root.left) && recurse(root.right);
+        if(root.data != (rightVal + leftVal)){
+            return false;
         }
+        return isSumProperty(root.left) && isSumProperty(root.right);
         
-        return false;
+        
+        
     }
+    
+    
 }
