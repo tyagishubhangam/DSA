f5d9a0ab0057d2226c98928db25f06e2d71f3959
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2026-03-22 01:45:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -2,7 +2,8 @@ class Solution {
     public int countSeniors(String[] details) {
         int cnt= 0;
         for(String s : details){
-            if(Integer.parseInt(s.substring(s.length()-4, s.length()-2)) > 60){
+            int n = s.length();
+            if(s.charAt(n-4) > '6' || (s.charAt(n-4) == '6' && s.charAt(n-3) > '0')){
                 cnt++;
             }
         }
