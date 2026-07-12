4cb5562332f1527ec247e5d4cc4bf88ffda3d8e1
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2026-07-13 01:53:07 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,23 +2,22 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        for(int i=0;i<n;i++){
-            dp[i] = -1;
-        }
-        return getAmount(0,dp,nums);
+        Arrays.fill(dp, -1);
+        return Math.max(recurse(0, nums, dp), recurse(1,nums, dp)); 
+
     }
 
-    public static int getAmount(int idx,int[] dp, int[] arr){
-        if(idx >= arr.length){
+    public int recurse(int i, int[] nums, int[] dp){
+        if(i >= nums.length){
             return 0;
         }
-        if(dp[idx] != -1){
-            return dp[idx];
+        if(dp[i] != -1){
+            return dp[i];
         }
-        int incl = arr[idx] + getAmount(idx+2,dp,arr);
-        int excl = getAmount(idx + 1, dp, arr);
-        dp[idx] = Math.max(incl, excl);
-        return dp[idx];
-        
+        int take = nums[i]+ recurse(i+2, nums, dp);
+        int notTake = recurse(i+1, nums, dp);
+
+        return dp[i] = Math.max(take, notTake);
+
     }
 }
