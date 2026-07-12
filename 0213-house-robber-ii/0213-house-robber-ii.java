5d226263105358f5da3e10e0ce329123463e0426
Shubhangam Tyagi <143538952+tyagishubhangam@users.noreply.github.com>
2026-07-13 02:33:39 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,32 +1,44 @@
 class Solution {
     public int rob(int[] nums) {
-        
-        int prevPrev = nums[0];
-        if(nums.length == 1){
-            return prevPrev;
+        int n = nums.length;
+        int[] dp = new int[n];
+        if(n == 1){
+            return nums[0];
         }
-        if(nums.length == 2){
-            return Math.max(nums[0], nums[1]);
+        Arrays.fill(dp, -1);
+        int path1 = recurse(0, nums, dp);
+        if(n <= 1){
+            return path1;
         }
-        int prev = Math.max(prevPrev,nums[1]);
-        int robFromFirst = 0;
-        for(int i=2;i<nums.length-1;i++){
-            int curr = Math.max(prevPrev + nums[i], prev);
-            prevPrev = prev;
-            prev = curr;
-        }
-        robFromFirst = prev;
+        Arrays.fill(dp, -1);
+        int path2 = recurse2(1, nums, dp);
 
-        prevPrev = nums[1];
-        prev = Math.max(prevPrev,nums[2]);
-        int robFromSecond = 0;
-        for(int i=3;i<nums.length;i++){
-            int curr = Math.max(prevPrev + nums[i], prev);
-            prevPrev = prev;
-            prev = curr;
-        }
-        robFromSecond = prev;
-
-        return Math.max(robFromFirst, robFromSecond);
+        return Math.max(path1, path2);
     }
+
+    public int recurse(int i, int[] nums, int[] dp){
+        if(i >= nums.length-1){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int take = nums[i] + recurse(i+2, nums, dp);
+        int notTake = recurse(i+1, nums, dp);
+        return dp[i] = Math.max(take, notTake);
+    }
+    
+    public int recurse2(int i, int[] nums, int[] dp){
+        if(i >= nums.length){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int take = nums[i] + recurse2(i+2, nums, dp);
+        int notTake = recurse2(i+1, nums, dp);
+        return dp[i] = Math.max(take, notTake);
+    }    
 }
